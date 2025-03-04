6eaa0552e49254c4ffc9404b647631eb71fc9a86
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-03-04 21:25:27 +0500

    feat (frontend): Make chat input form responsive

--- components/ChatForm.tsx
+++ components/ChatForm.tsx
@@ -15,15 +15,18 @@ const ChatForm = ({
     }
   };
   return (
-    <form className="flex gap-2 mt-4" onSubmit={handleSubmit}>
+    <form
+      className="flex flex-col gap-0 mt-4 sm:flex-row sm:gap-2"
+      onSubmit={handleSubmit}
+    >
       <input
-        className="flex-1 px-4 py-2 border-2 rounded-lg focus:outline-none"
+        className="flex-1 px-4 py-2 border-2 mb-2 rounded-lg focus:outline-none sm:mb-0 sm:w-auto"
         onChange={(e) => setMessage(e.target.value)}
         type="text"
         placeholder="Placeholder..."
       />
       <button
-        className="px-4 py-2 rounded-lg bg-blue-500 text-white"
+        className="px-4 py-2 rounded-lg bg-blue-500 text-white w-full sm:w-auto"
         type="submit"
       >
         Send
