088601ae605e752fb0a0dacebcb8d98ae3cf52b7
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-03-07 15:47:06 +0500

    feat (frontend): Implement chat room functionality with user input and message display

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,66 @@
 "use client";
+import React from "react";
+import { useState } from "react";
 import ChatForm from "@/components/ChatForm";
-// import Image from "next/image";
+import ChatMessage from "@/components/ChatMessage";
 
 export default function Home() {
+  const handleJoinRoom = () => {
+    setJoined(true);
+  };
+  const [room, setRoom] = useState("");
+  const [joined, setJoined] = useState(false);
+  const [messages, setMessages] = useState<
+    { sender: string; message: string }[]
+  >([]);
+  const [userName, setUserName] = useState("");
   const handleSendMessage = (message: string) => {
     console.log(message);
   };
   return (
     <div className="flex mt-24 justify-center w-full">
-      <div className="w-full max-w-3xl mx-auto">
-        <h1 className="mb-4 font-bold text-2xl">Room 1</h1>
-        <div>{/* TODO: Add chat room */}</div>
-        <ChatForm onSendMessage={handleSendMessage} />
-      </div>
+      {!joined ? (
+        <div className="flex w-full max-w-3xl mx-auto flex-col items-center">
+          <h1 className="mb-4 text-2xl font-bold">Join a Room</h1>
+          {/* username */}
+          <input
+            type="text"
+            placeholder="Enter you username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            className="w-64 px-4 mb-4 border-2 rounded-lg"
+          />
+          {/* room */}
+          <input
+            type="text"
+            placeholder="Enter room name"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+            className="w-64 px-4 mb-4 border-2 rounded-lg"
+          />
+          <button
+            onClick={handleJoinRoom}
+            className="px-4 py-2 text-white bg-blue-500 rounded-lg"
+          >
+            Join Room
+          </button>
+        </div>
+      ) : (
+        <div className="w-full max-w-3xl mx-auto">
+          <h1 className="mb-4 font-bold text-2xl">Room 1</h1>
+          <div className="h-[500] overflow-y-auto p-4 mb-4 bg-gray-200 border-2 rounded-lg">
+            {messages.map((msg, index) => (
+              <ChatMessage
+                key={index}
+                sender={msg.sender}
+                message={msg.message}
+                isOwnMessage={msg.sender === userName}
+              />
+            ))}
+          </div>
+          <ChatForm onSendMessage={handleSendMessage} />
+        </div>
+      )}
     </div>
   );
 }
