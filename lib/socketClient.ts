175f2ccc98c779e33d2941f19ace9ba995795243
sharipovikromjon <ikromjonsharipov924@gmail.com>
2025-04-19 00:22:50 +0500

    feat: Configure socket connection to use dynamic backend URL with credentials

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -1,3 +1,8 @@
 "use client";
 import { io } from "socket.io-client";
-export const socket = io();
+
+const BACKEND_URL = process.env.NODE_ENV === "production" ? "https://web-chat-app-11d9ba475cb9.herokuapp.com" : undefined;
+
+export const socket = io(BACKEND_URL, {
+  withCredentials: true,
+});
